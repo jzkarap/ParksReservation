9201ce9e83d3805fab4e55d1cd5e737c0fed2b38
Daniel Medina <ShenFan001@GMail.com>
2018-06-27 13:04:42 -0400

    Create Campground obj w/properties

---
+++ Capstone/Models/Campground.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Campground
+    {
+        public int Campground_Id { get; set; }
+
+        public int Park_Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int OpenMonth { get; set; }
+
+        public int CloseMonth { get; set; }
+
+        public double DailyFee { get; set; }
+    }
+}
